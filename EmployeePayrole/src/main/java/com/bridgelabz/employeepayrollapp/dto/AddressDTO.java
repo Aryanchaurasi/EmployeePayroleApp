fe35d90be1e5fe4eca7baedf1a88d9d1b504f9fc
Aryan chaurasia <aryachaurasi20@gmail.com>
2025-02-27 11:16:21 +0530

    Aryan chaurasia: add- Provided User Friendly Error Response in case validation fails

--- EmployeePayrole/src/main/java/com/bridgelabz/employeepayrollapp/dto/AddressDTO.java
+++ EmployeePayrole/src/main/java/com/bridgelabz/employeepayrollapp/dto/AddressDTO.java
@@ -17,6 +17,9 @@ public class AddressDTO {
     @Pattern(regexp = "^[A-Z][a-zA-Z ]{2,}$", message = "Name must start with a capital letter and have at least 3 characters")
     private String name;
     private String id;
+    @NotBlank(message = "Phone number  cannot be empty")
     private String phoneNumber;
+    @NotBlank(message = "Email cannot be empty")
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}")
     private String email;
 }
--- EmployeePayrole/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeeModel.java
+++ EmployeePayrole/src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeeModel.java
@@ -1,49 +1,68 @@
 package com.bridgelabz.employeepayrollapp.model;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Valid
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
+    @NotBlank(message = "Name cannot be empty")
     public String name;
+    @NotBlank(message = "Department cannot be empty")
     public String department;
+    @NotBlank(message = "Sallary cannot be empty")
     public double salary;
 
-    // Constructors
-    public EmployeeModel() {}
-
-    public EmployeeModel(String name, String department, double salary) {
-        this.name = name;
-        this.department = department;
-        this.salary = salary;
-    }
-
-    // Getters & Setters
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
+//    // Constructors
+//    public EmployeeModel() {}
+//
+//    public EmployeeModel(String name, String department, double salary) {
+//        this.name = name;
+//        this.department = department;
+//        this.salary = salary;
+//    }
+//
+//    // Getters & Setters
+//    public Long getId() {
+//        return id;
+//    }
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getDepartment() {
+//        return department;
+//    }
+//    public void setDepartment(String department) {
+//        this.department = department;
+//    }
+//
+//    public double getSalary() {
+//        return salary;
+//    }
+//    public void setSalary(double salary) {
+//        this.salary = salary;
+//    }
 }
